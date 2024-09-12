47c653d0a46b564393cc76a6944e7bbbc5241e2f
MostafaShebl1 <mostafa.medhat.shebl9@gmail.com>
2024-09-12 19:24:25 +0300

    ver1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define MACRO 4
+
+int main(void)
+{
+  int x=0,y=0;
+  y=MACRO + x;
+
+   while(1){
+            
+            printf("y=%d\n",y);
+          }
+
+  return 0;
+
+}
